1d92e28a131c7f4cb1c36492091bf9a15befc2ca
Aser <asermalik@live.com>
2019-07-23 17:36:20 -0400

    negative radians issue fixed, new momentum calculation error discovered

--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -7,8 +7,8 @@ var player_weight = 10
 var velocity = Vector2(0,0)
 var r_velocity = Vector2(0,0)
 
-var item_inventory = [0,1,2,3,4,5]
-var item_weight = [5,10,15,20,25,30]
+var item_inventory = [0,6,1,2,3,4,5]
+var item_weight = [5,5,10,15,20,25,30]
 
 func get_items_weight():
 	var sum = 0;
@@ -33,16 +33,18 @@ func get_input():
 		velocity = r_velocity
 
 	if Input.is_action_pressed('right'):
-		rotation_dir += 1
+		rotation_dir += 0.5
 		print (rotation)
-		print ("sin: ", sin(rotation))
+		print ("sin: ", sin(rotation), " - cos: ", cos(rotation))
 	if Input.is_action_pressed('left'):
-		rotation_dir -= 1
+		rotation_dir -= 0.5
 		print (rotation)
+		print ("sin: ", sin(rotation), " - cos: ", cos(rotation))
 	if Input.is_action_just_pressed('main_action'):
 		onPress_rotation_dir = rotation
 
 		item_thrown_speed = 25
+
 		item_thrown_angleX = sin(rotation)*(-1)
 		item_thrown_angleY = cos(rotation)
 
@@ -51,19 +53,23 @@ func get_input():
 
 		velocity[0] = (momentumX - (item_weight[0]*item_thrown_angleX*scale_factor))/player_weight
 		velocity[1] = (momentumY - (item_weight[0]*item_thrown_angleY*scale_factor))/player_weight
-		#problem: radians only go to 3.14 before flipping negative
 
 		item_inventory.pop_front()
 		item_weight.pop_front()
 
+		momentumX = (player_weight+get_items_weight())*velocity[0]
+		momentumY = (player_weight+get_items_weight())*velocity[1]
+		#problem: new momentum not calculated correctly!
+
 		print ("post: ", velocity[0], " ", velocity[1])
 
-		velocity = Vector2(velocity[0], velocity[1]).rotated(onPress_rotation_dir)
+		velocity = Vector2(velocity[0], velocity[1])
+
 		r_velocity = velocity
 	#if Input.is_action_just_released("main_action"):
 		#print (get_items_weight())
 
 func _physics_process(delta):
 	get_input()
-	rotation += rotation_dir * rotation_speed * delta
+	rotation += rotation_dir * rotation_speed * delta * 2
 	velocity = move_and_collide(velocity)
