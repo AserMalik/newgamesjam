c52b04b7c4a417f1724a5c0f56a966e8fc3dfb26
Aser <asermalik@live.com>
2019-07-22 23:10:57 -0400

    momentum system added, needs bug fixing

--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -2,15 +2,13 @@ extends KinematicBody2D
 
 export (int) var speed = 200
 export (float) var rotation_speed = 1.5
+export (int) var scale_factor = 5
 
 var player_weight = 10
 var velocity = Vector2(0,0)
 
-var item_inventory = [0,1,2]
-var item_weight = [1,2,3]
-
-var rotation_dir = 0
-var onPress_rotation_dir = 0
+var item_inventory = [0,1,2,3,4,5]
+var item_weight = [5,10,15,20,25,30]
 
 func get_items_weight():
 	var sum = 0;
@@ -18,16 +16,19 @@ func get_items_weight():
 		sum += item_weight[i]
 	return sum
 
+var rotation_dir = 0
+var onPress_rotation_dir = 0
+
 var momentumX = (player_weight+get_items_weight())*velocity[0] #at start of action, player and items share velocity
 var momentumY = (player_weight+get_items_weight())*velocity[1] #use these to calc new speed
 
-var final_momentumX = 0;
-var final_momentumY = 0;
+var item_thrown_angleX = 0;
+var item_thrown_angleY = 0;
 
+var item_thrown_speed = 0;
 
 func get_input():
 	rotation_dir = 0
-	velocity = Vector2()
 	
 	if Input.is_action_pressed('right'):
 		rotation_dir += 1
@@ -35,19 +36,29 @@ func get_input():
 	if Input.is_action_pressed('left'):
 		rotation_dir -= 1
 		print (rotation)
-	#if Input.is_action_pressed('down'):
-		#velocity = Vector2(0, speed).rotated(rotation)
 	if Input.is_action_just_pressed('main_action'):
-		item_inventory.pop_front() #throw item
 		onPress_rotation_dir = rotation
-		
-		# velocity[0] = find the thrown objects x and y velocity first, then plug into equation
-		velocity = Vector2(0, -speed).rotated(onPress_rotation_dir)
-	if Input.is_action_just_released("main_action"):
 
-		print (get_items_weight())
+		item_thrown_speed = 25
+		item_thrown_angleX = sin(rotation)*(-1)
+		item_thrown_angleY = cos(rotation)*(-1)
+
+		print ("pre: ", velocity[0], " ", velocity[1])
+		print ("item popped: ", item_inventory[0], " ", item_weight[0])
+
+		velocity[0] = (momentumX - (item_weight[0]*item_thrown_angleX*scale_factor))/player_weight
+		velocity[1] = (momentumY - (item_weight[0]*item_thrown_angleY*scale_factor))/player_weight
+		
+		item_inventory.pop_front()
+		item_weight.pop_front()
+		
+		print ("post: ", velocity[0], " ", velocity[1])
+
+		velocity = Vector2(velocity[0], velocity[1]).rotated(onPress_rotation_dir)
+	#if Input.is_action_just_released("main_action"):
+		#print (get_items_weight())
 
 func _physics_process(delta):
-    get_input()
-    rotation += rotation_dir * rotation_speed * delta
-    velocity = move_and_collide(velocity)
+	get_input()
+	rotation += rotation_dir * rotation_speed * delta
+	velocity = move_and_collide(velocity)
